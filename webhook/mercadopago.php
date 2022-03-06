8e4449a92a7f0cb72e1657f110d2855e3dd281e2
sixtored <sixtod@gmail.com>
2022-03-06 19:59:23 -0300

    ...

--- webhook/mercadopago.php
+++ webhook/mercadopago.php
@@ -27,12 +27,12 @@ MercadoPago\SDK::setAccessToken($mpaccess_token);
 $json = file_get_contents('php://input');
 $info = json_decode($json);
 
-/*
+
 $sql = $con->prepare("INSERT INTO WEBHOOKS (type, info, action, live_mode)
             VALUE (?, ?, ?, ?)");
 
 $sql->execute(['pueba', $json, 'test', 1]);
-*/
+
 
 $collection_id = $info->resource;
 $data_id = substr($collection_id, 13);
@@ -134,6 +134,8 @@ if (isset($info->topic)) {
                     $sql->execute([$id]);
                     if ($sql->fetchColumn() > 0) {
                         // existe el item pagado.. actualizamos el pago.. del abono 
+                        $sql = $con->prepare("SELECT id, idcta, idabonado, pagado, periodo, nombre FROM CTABOTONPAGO WHERE id = ? and pagado = 0");
+                        $sql->execute([$id]);
                         $dato = $sql->fetch(PDO::FETCH_ASSOC);
                         $nombre = $dato['nombre'] . '[' . $dato['periodo'] . ']';
                         $idabonado = $dato['idabonado'];
@@ -159,10 +161,12 @@ if (isset($info->topic)) {
                         $sql_insert->execute([$idcobro, $idabonado, $periodo, $subtotal, $detalle,  $fch, $idcta, $id]);
                     }
                 }
-                return http_response_code(200);
+                http_response_code(200);
+                return 200 ;
             } else {
                 // no existe el id del pago..
-               return http_response_code(200);
+                http_response_code(200);
+                return 200 ;
                 $email = '';
                 //$status = '' ;
                 $payment_method = '';
@@ -171,13 +175,14 @@ if (isset($info->topic)) {
             }
 
            
-
             break;
 
         default:
             $sql = $con->prepare("INSERT INTO WEBHOOKS (type, info, action, live_mode)
             VALUE (?, ?, ?, ?)");
             $sql->execute(['default', $json, 'test', 1]);
+            http_response_code(201);
+            return 201 ;
     }
 } else {
     if (isset($info->type)) {
@@ -195,91 +200,8 @@ if (isset($info->topic)) {
         $type           = $info->type;
         $action         = $info->action;
         $sql->execute([$id, $live_mode, $application_id, $user_id, $version, $api_version, $type, $action, $json]);
+        http_response_code(201);
+        return 201 ;
     }
 }
-
-/*
-$sql = $con->prepare("INSERT INTO WEBHOOKS (id_mp, live_mode, aplication_id, user_id, version, api_version, type, action, info)
-                VALUE (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-if ($info->live_mode = "true") $live_mode = 1;
-else $live_mode = 0;
-$id             = $info->id;
-$application_id = $info->application_id;
-$user_id        = $info->user_id;
-//$version        = $info->version ;
-$version = '22';
-$api_version    = $info->api_version;
-$type           = $info->type;
-$action         = $info->action;
-$sql->execute([$id, $live_mode, $application_id, $user_id, $version, $api_version, $type, $action, $json]);
-
-
-echo http_response_code(200);
-
-
-
-if (isset($info->type)) {
-    switch ($info->type) {
-        case 'mp-connect':
-            // Desvinculo de mi sistema cuando el usuario desautoriza la app desde su cuenta de Mercadopago.
-            if ($info->action == 'application.deauthorized') {
-
-                $data_update = array(
-                    'mp_access_token' => NULL,
-                    'mp_public_key' => NULL,
-                    'mp_refresh_token' => NULL,
-                    'mp_user_id' => NULL,
-                    'mp_expires_in' => NULL,
-                    'mp_status' => 0
-                );
-
-                // $this->producers->update_mp_connect($data_update, $info->user_id);
-                // $this->output->set_status_header(200);
-                //updated_users($data_update,$info->user_id) ;
-                //var_dump($data_update) ;
-                $archivo = fopen('appication.txt', 'w+');
-                fwrite($fh, $info);
-                fclose($archivo);
-                echo http_response_code(200);
-                return;
-            }
-
-            // Pueden tomar otra acción si el $info->action = 'application.authrized'
-            break;
-
-        case 'payment':
-            // Actualizo la información de pago recibida.
-            $or_collection_id = $info->data->id;
-            $info = MercadoPago\Payment::find_by_id($or_collection_id);
-            $or_number = $info->external_reference;
-
-            $data_update = array(
-                'or_collection_status' => $info->status,
-                'or_collection_status_detail' => $info->status_detail,
-                'or_payment_type' => $info->payment_type_id,
-                'or_payment_method' => $info->payment_method_id,
-                'or_status' => ''
-            );
-            //gcfg($info->status,'or_status_collection_status')
-
-            $archivo = fopen('payment.txt', 'w+');
-            fwrite($fh, $info);
-            fclose($archivo);
-            echo http_response_code(200);
-
-            // $this->cart->update_ipn_order($data_update,$or_number);
-
-            break;
-
-        default:
-            $archivo = fopen('default.txt', 'w+');
-            fwrite($fh, $info);
-            fclose($archivo);
-            echo http_response_code(200);
-            return;
-            break;
-    }
-}
-echo http_response_code(200);
-return;
-*/
+?>
