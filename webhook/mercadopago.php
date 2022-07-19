0287334325f66748985d8487bcd7cb03bc9e8526
sixtored <sixtod@gmail.com>
2022-07-19 11:51:54 -0300

    prueba..

--- webhook/mercadopago.php
+++ webhook/mercadopago.php
@@ -357,12 +357,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         //echo http_response_code(201);
                         //echo json_encode($res->getResponse("(CREATED)", " ", 201, "Type no existe.."));
                         
+                        echo http_response_code(200);
+
                         $email = 'sixtored@hotmail.com' ;
                         $noti = 'Notificacion Test';
                         include 'enviar_email.php';
-                        
-                        echo http_response_code(200);
-
                        
   
                         exit(1);
